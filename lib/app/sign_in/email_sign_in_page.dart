e55569a2b0bba96de77245420f2a8c59266ee97b
fabiovalinhos <fabiovalinhos@gmail.com>
2021-07-24 19:54:25 -0300

    Bloc login applied in email sign in form

--- lib/app/sign_in/email_sign_in_page.dart
+++ lib/app/sign_in/email_sign_in_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:timer_tracker_flutter_course/app/sign_in/email_sign_in_form.dart';
+import 'package:timer_tracker_flutter_course/app/sign_in/email_sign_in_form_stateful.dart';
 
 class EmailSignInPage extends StatelessWidget {
   @override
@@ -13,7 +13,7 @@ class EmailSignInPage extends StatelessWidget {
         scrollDirection: Axis.vertical,
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Card(child: EmailSignInForm()),
+          child: Card(child: EmailSignInFormStateful()),
         ),
       ),
       backgroundColor: Colors.grey[200],
