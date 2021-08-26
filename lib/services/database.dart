a69d106dbfa4a3084b06b32f5a60d6254441ae35
fabiovalinhos <fabiovalinhos@gmail.com>
2021-08-26 07:57:10 -0300

    File for api paths

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:timer_tracker_flutter_course/app/home/models/job.dart';
+import 'package:timer_tracker_flutter_course/services/api_path.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
@@ -12,7 +13,10 @@ class FirestoreDatabase implements Database {
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
 
   Future<void> createJob(Job job) async {
-    final path = '/users/$uid/jobs/jobs_abc';
+    final path = APIPath.job(
+      uid,
+      'job_abc',
+    );
     final documentReference = FirebaseFirestore.instance.doc(path);
     await documentReference.set(job.toMap());
   }
