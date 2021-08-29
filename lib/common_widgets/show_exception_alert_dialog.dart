90a3cf5ad7875f8e918301cf8c147ce84c45a81a
fabiovalinhos <fabiovalinhos@gmail.com>
2021-08-29 11:47:33 -0300

    put method for reading firestore's data jobs

--- lib/common_widgets/show_exception_alert_dialog.dart
+++ lib/common_widgets/show_exception_alert_dialog.dart
@@ -2,8 +2,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:timer_tracker_flutter_course/common_widgets/show_alert_dialog.dart';
 
-Future<void> showExceptionAlertDialog(BuildContext context,
-        {@required String title, @required Exception exception}) =>
+Future<void> showExceptionAlertDialog(
+  BuildContext context, {
+  @required String title,
+  @required Exception exception,
+}) =>
     ShowAlertDialog(
       context,
       title: title,
