77dcffe8da1c791b83a89b03130efbe9b0946006
fabiovalinhos <fabiovalinhos@gmail.com>
2021-09-15 19:50:34 -0300

    put Provider Database in add_job_page

--- lib/app/home/jobs/add_job_page.dart
+++ lib/app/home/jobs/add_job_page.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:timer_tracker_flutter_course/services/database.dart';
 
 class AddJobPage extends StatefulWidget {
+  const AddJobPage({Key key, @required this.database}) : super(key: key);
+  final Database database;
+
   static Future<void> show(BuildContext context) async {
+    // Ele pegou pelo context o Provider do jobs_page
+    final database = Provider.of<Database>(context, listen: false);
     await Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => AddJobPage(),
+          builder: (context) => AddJobPage(
+            database: database,
+          ),
           fullscreenDialog: true,
         ));
   }
@@ -15,12 +24,46 @@ class AddJobPage extends StatefulWidget {
 }
 
 class _AddJobPageState extends State<AddJobPage> {
+  final _formkey = GlobalKey<FormState>();
+
+  String _name;
+  int _ratePerHour;
+
+  bool _validateAndSaveForm() {
+    final form = _formkey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  void _submit() {
+    if (_validateAndSaveForm()) {
+      print('Form saved, name: $_name, ratePerHour: $_ratePerHour');
+    }
+
+    final database = Provider.of<Database>(context, listen: false);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 2.0,
         title: Text('New Job'),
+        actions: <Widget>[
+          TextButton(
+            child: Text(
+              'Save',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
+            ),
+            onPressed: _submit,
+          ),
+        ],
       ),
       body: _buildContents(),
       backgroundColor: Colors.grey[200],
@@ -29,18 +72,48 @@ class _AddJobPageState extends State<AddJobPage> {
 
   Widget _buildContents() {
     return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
+      // scrollDirection: Axis.vertical,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Card(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Placeholder(
-              fallbackHeight: 200,
-            ),
+            child: _buildForm(),
           ),
         ),
       ),
     );
+  }
+
+  Widget _buildForm() {
+    return Form(
+      key: _formkey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: _BuildFormChildren(),
+      ),
+    );
+  }
+
+  List<Widget> _BuildFormChildren() {
+    return [
+      TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Job name',
+        ),
+        onSaved: (value) => _name = value,
+        validator: (value) => value.isNotEmpty ? null : 'Name can\'t be empty',
+      ),
+      TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Rate per hour',
+        ),
+        keyboardType: TextInputType.numberWithOptions(
+          signed: false,
+          decimal: false,
+        ),
+        onSaved: (value) => _ratePerHour = int.parse(value) ?? 0,
+      ),
+    ];
   }
 }
