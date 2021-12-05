d5c9bf875d85612a81faca294c5318c2c2d478b0
fabiovalinhos <fabiovalinhos@gmail.com>
2021-12-05 18:07:27 -0300

    put pickers in entry ... adjusts

--- lib/app/home/job_entries/entry_page.dart
+++ lib/app/home/job_entries/entry_page.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:timer_tracker_flutter_course/app/home/job_entries/date_time_picker.dart';
+import 'package:timer_tracker_flutter_course/common_widgets/date_time_picker.dart';
 import 'package:timer_tracker_flutter_course/app/home/job_entries/format.dart';
 import 'package:timer_tracker_flutter_course/app/home/models/entry.dart';
 import 'package:timer_tracker_flutter_course/app/home/models/job.dart';
@@ -122,8 +122,8 @@ class _EntryPageState extends State<EntryPage> {
       labelText: 'Start',
       selectedDate: _startDate,
       selectedTime: _startTime,
-      selectDate: (date) => setState(() => _startDate = date),
-      selectTime: (time) => setState(() => _startTime = time),
+      onSelectedtDate: (date) => setState(() => _startDate = date),
+      onSelectedTime: (time) => setState(() => _startTime = time),
     );
   }
 
@@ -132,8 +132,8 @@ class _EntryPageState extends State<EntryPage> {
       labelText: 'End',
       selectedDate: _endDate,
       selectedTime: _endTime,
-      selectDate: (date) => setState(() => _endDate = date),
-      selectTime: (time) => setState(() => _endTime = time),
+      onSelectedtDate: (date) => setState(() => _endDate = date),
+      onSelectedTime: (time) => setState(() => _endTime = time),
     );
   }
 
--- lib/app/home/jobs/edit_job_page.dart
+++ lib/app/home/jobs/edit_job_page.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:timer_tracker_flutter_course/app/home/models/job.dart';
 import 'package:timer_tracker_flutter_course/common_widgets/show_alert_dialog.dart';
 import 'package:timer_tracker_flutter_course/common_widgets/show_exception_alert_dialog.dart';
