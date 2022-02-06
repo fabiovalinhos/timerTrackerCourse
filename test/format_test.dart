e46ac3c8064bc030fc2c0eef3ab4b464818f70b5
fabiovalinhos <fabiovalinhos@gmail.com>
2022-02-06 11:01:52 -0300

    finish format test

--- test/format_test.dart
+++ test/format_test.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:timer_tracker_flutter_course/app/home/job_entries/format.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() {
   group('hours', () {
@@ -15,6 +17,55 @@ void main() {
 
     test('decimal', () {
       expect(Format.hours(4.5), '4.5h');
+    });
+  });
+
+  group('date - GB Locale', () {
+    setUp(() async {
+      Intl.defaultLocale = 'en_GB';
+      await initializeDateFormatting(Intl.defaultLocale, '');
+    });
+
+    test('2022-01-31', () {
+      expect(Format.date(DateTime(2022, 01, 31)), '31 Jan 2022');
+    });
+  });
+
+  group('dayOfWeek - GB locale', () {
+    setUp(() async {
+      Intl.defaultLocale = 'en_GB';
+      await initializeDateFormatting(Intl.defaultLocale, '');
+    });
+
+    test('Monday', () {
+      expect(Format.dayOfWeek(DateTime(2022, 01, 31)), 'Mon');
+    });
+  });
+
+  group('dayOfWeek - IT locale', () {
+    setUp(() async {
+      Intl.defaultLocale = 'it_IT';
+      await initializeDateFormatting(Intl.defaultLocale, '');
+    });
+
+    test('Ludeni', () {
+      expect(Format.dayOfWeek(DateTime(2022, 01, 31)), 'lun');
+    });
+  });
+
+  group('currency - US locale', () {
+    setUp(() {
+      Intl.defaultLocale = 'en_US';
+    });
+
+    test('positive', () {
+      expect(Format.currency(10), '\$10');
+    });
+    test('zero', () {
+      expect(Format.currency(0), '');
+    });
+    test('negative', () {
+      expect(Format.currency(-5), '-\$5');
     });
   });
 }
