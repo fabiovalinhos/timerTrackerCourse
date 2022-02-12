ad26e85bcbc607ffc660313f8ddeaf7b707a16c2
fabiovalinhos <fabiovalinhos@gmail.com>
2022-02-12 18:09:16 -0300

    start widget's tests and updated kotlin and gradle for new flutter version

---
+++ test/custom_raised_button_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:timer_tracker_flutter_course/common_widgets/custom_raised_button.dart';
+
+void main() {
+  testWidgets('', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: CustomRaisedButton()));
+  });
+}
